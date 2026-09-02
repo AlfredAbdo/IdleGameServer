fa862cab01c70cf6bd3ec62822dc36abab6c8d04
Alfred Abdo <alfredabdo13@gmail.com>
2026-09-02 12:16:08 +0300

    Revert "- Fixed message of achievement for the infinite animation"
    This reverts commit 672fbcb1371595fd4231bf208f50dbfdfe1ffedf.

--- server/src/main/kotlin/data/values/Achievements.kt
+++ server/src/main/kotlin/data/values/Achievements.kt
@@ -2,7 +2,6 @@ package alfredabdo.ktor.idlegame.data.values
 
 import alfredabdo.ktor.idlegame.data.Achievement
 import alfredabdo.ktor.idlegame.data.AchievementCondition
-import alfredabdo.ktor.idlegame.format.GameFormatter
 
 val achievements: List<Achievement>
     get() = listOf(
@@ -24,7 +23,7 @@ val achievements: List<Achievement>
         ),
         Achievement(
             3u,
-            "Get any item duration to ${GameFormatter.formatDuration(infiniteAnimationThreshold)} or below",
+            "Get any item duration to $infiniteAnimationThreshold or below",
             "NOT THE EYES!",
             listOf(
                 AchievementCondition.anyItem { fillRate { leq(infiniteAnimationThreshold.inWholeMilliseconds) } },
